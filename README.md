332e742116394f127fab86367e2ce6d44fcfaf10
Turbobot <turbobot@vercel.com>
2025-06-27 23:33:12 +0900

    feat(create-turbo): apply official-starter transform

---
+++ README.md
@@ -0,0 +1,1 @@
+### cllaude99_lib
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,23 @@
+import { Counter, Header } from '@repo/ui';
+import typescriptLogo from '/typescript.svg';
+
+const App = () => (
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" className="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img
+        src={typescriptLogo}
+        className="logo vanilla"
+        alt="TypeScript logo"
+      />
+    </a>
+    <Header title="Web" />
+    <div className="card">
+      <Counter />
+    </div>
+  </div>
+);
+
+export default App;
