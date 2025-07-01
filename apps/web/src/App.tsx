647359b3755d5eb631656b3fe620092984545597
Cllaude99 <xodbsrla1025@naver.com>
2025-07-01 13:49:11 +0900

    feat: landing path 추가

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,10 +4,15 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Cllaude99UIProvider } from '@cllaude99_lib/ui';
 import DefaultComponent from './components/DefaultComponent';
+import LandingPage from './pages/Landing';
 
 const router = createBrowserRouter([
   {
     index: true,
+    element: <LandingPage />,
+  },
+  {
+    path: '/default',
     element: <DefaultComponent />,
   },
 ]);
